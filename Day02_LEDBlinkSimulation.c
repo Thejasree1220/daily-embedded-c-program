66144ba9fbe75052199c13d3f399597c853430b5
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-18 23:48:51 +0530

    Day 2: LED Blink Simulation in Embedded C
    Initialize LED output and create a blinking effect.

---
+++ Day02_LEDBlinkSimulation.c
@@ -0,0 +1,31 @@
+#include<lpc21xx.h>
+void delay_ms(unsigned int ms);
+void delay_sec(unsigned int sec);
+int main()
+{
+  IODIR0=0xff;
+  IOSET0=0xff;
+  delay_ms(500);
+  while(1)
+    {
+      IOCLR0=0xff;
+      delay_ms(500);
+      IOSET0=0xff;
+      delay_ms(500);
+}
+void delay_sec(unsigned int sec)
+{
+  T0PR=15000000-1;
+T0PC=T0TC=0;
+T0TCR=1;
+while(T0TC<sec);
+T0TCR=0;
+}
+void delay_ms(unsigned int sec)
+{
+  T0PR=15000-1;
+T0PC=T0TC=0;
+T0TCR=1;
+while(T0TC<ms);
+T0TCR=0;
+}
