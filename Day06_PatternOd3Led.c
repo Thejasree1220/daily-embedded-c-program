75d5151c010259cdb741b2747e0fe5689b55e825
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-23 23:32:55 +0530

    Day 6: Pattern Of 3 LED in Embedded C
    Implement LED pattern control with delays for LPC21xx.

---
+++ Day06_PatternOd3Led.c
@@ -0,0 +1,37 @@
+#include<lpc21xx.h>
+void delay_ms(unsigned int ms);
+void delay_sec(unsigned int sec);
+int main()
+{
+	int i,temp=0;
+  IODIR0=7;
+  IOSET0=7;
+  delay_ms(100);
+  while(1)
+    {
+			for(i=3;i>=0;i--)
+			{
+					temp=(1<<i)-1;
+					IOCLR0=temp;
+					delay_ms(100);	
+					IOSET0=temp;
+					delay_ms(100);
+			}
+	  }
+}
+void delay_sec(unsigned int sec)
+{
+  T0PR=15000000-1;
+T0PC=T0TC=0;
+T0TCR=1;
+while(T0TC<sec);
+T0TCR=0;
+}
+void delay_ms(unsigned int ms)
+{
+  T0PR=15000-1;
+T0PC=T0TC=0;
+T0TCR=1;
+while(T0TC<ms);
+T0TCR=0;
+}
